f1c353392afbcf2ed438b701ea16b332129f240c
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-01-03 00:09:14 -0300

    creating check-in Use Case - part 2

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,15 +1,17 @@
-import { Prisma, User } from '@prisma/client'
+import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-  public items: User[] = []
+  public items: CheckIn[] = []
 
-  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<User> {
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
       gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
     }
 
     this.items.push(checkIn)
