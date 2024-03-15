1ac290647ce4b78c1e3c05b56bd8aadc0f9a48b6
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-03-14 23:21:28 -0300

    creating the check-ins controllers

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,33 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+
+  const createCheckInBodySchema = z.object({
+    // The refine method below is a function to use a verification that is not native from zod like min or max
+    latitude: z.coerce.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.coerce.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const checkInUseCase = makeCheckInUseCase()
+
+  await checkInUseCase.execute({
+    gymId,
+    userId: request.user.sub, // getting the userId from the authenticated JWT token
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send()
+}
---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  // All query params from req.query are strings, so we will use coerce from zod
+  // to convert from string to number
+  const checkInHistoryQuerySchema = z.object({
+    // if page does not exist in req.query it will be by default 1:
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page } = checkInHistoryQuerySchema.parse(request.query)
+
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+    userId: request.user.sub,
+    page,
+  })
+
+  return reply.status(200).send({ checkIns })
+}
